90aee739995be8c3122551cbeec32f10716d8e3e
Jason Desiderio <jason@placetostandagency.com>
2026-01-22 12:56:21 -0600

    Update homepage title to lowercase for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center font-mono text-center">
-        <h1 className="text-4xl font-bold uppercase">So Many Cults</h1>
+        <h1 className="text-4xl font-bold">so many cults</h1>
         <div className="text-[#666]">Austin, Texas Psych Rock</div>
         <ul className="space-y-4">
           <li><Link href="mailto:somanycults@gmail.com">Contact</Link></li>
