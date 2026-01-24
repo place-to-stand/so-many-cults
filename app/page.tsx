b7fbcf4c750a2255ce5ed77b00a87668379f54b6
Jason Desiderio <jason@placetostandagency.com>
2026-01-23 22:12:24 -0600

    Update homepage links to include Upcoming Shows and reorder contact information

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,10 @@ export default function Home() {
         <h1 className="text-4xl font-bold">so many cults</h1>
         <div className="text-[#666]">Austin, Texas Psych Rock</div>
         <ul className="space-y-4">
-          <li><Link href="mailto:somanycults@gmail.com">Contact</Link></li>
+          <li><Link href="https://do512.com/artists/so-many-cults" target="_blank">Upcoming Shows</Link></li>
           <li><Link href="https://instagram.com/somanycults" target="_blank">Instagram</Link></li>
           <li><Link href="https://somanycults.bandcamp.com/" target="_blank">Bandcamp</Link></li>
+          <li><Link href="mailto:somanycults@gmail.com">Contact</Link></li>
         </ul>
       </main>
     </div>
